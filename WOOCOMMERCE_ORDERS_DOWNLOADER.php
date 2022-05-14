08a640c900936ae82b08244e8aae09a05ae92c8b
Ex <jtalero10@outlook.es>
2022-05-14 00:01:55 -0500

    Get Woocommerce dependency, we need display alert when woocommerce isn't active

--- WOOCOMMERCE_ORDERS_DOWNLOADER.php
+++ WOOCOMMERCE_ORDERS_DOWNLOADER.php
@@ -17,30 +17,77 @@
  */
 
 /**
- *
+ * WOD
 **/
-
  ///ENQUEUE SCRIPT FOR LOCAL STORAGE 
  defined('ABSPATH') or die ('Unauthorized Access');
 
-function WOD_init(){
+// use Controllers\adminPage\WodMainController;
 
+
+
+function WOD_admin_menu(){
+
+	// $controller = new WodMainController;
         /***
         **** Admin menu bar
         ***/
 
-        $page_title = "Woocommerce Orders Downloader";
-        
-        $menu_title = "Orders";
-        
-        $capability = "Editor";
-        
-        $menu_slug = "woocommerce-orders-downloader";
-
-        $position = 1;
-
-        add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function = '', $icon_url = '', $position );
+    add_menu_page(
+		__( 'Woocommerce Orders Downloader', 'woocommerce-orders-downloader' ),
+		__( 'Orders', 'woocommerce-orders-downloader' ),
+		'manage_options',
+		'woocommerce-orders-downloader',
+		'my_admin_page_contents',
+		'dashicons-pdf',
+		3
+	);
 
 }
 
-register_activation_hook( __DIR__, "WOD_init" );
+add_action( "admin_menu" , "WOD_admin_menu" );
+
+/**
+ * Creamos la vista de administrador
+ * 
+ * @access public
+ * @return void
+ */
+function my_admin_page_contents() {
+		?>
+			<h1>
+				<?php esc_html_e( 'Welcome to my custom admin page.', 'my-plugin-textdomain' ); ?>
+			</h1>
+		<?php
+
+		if ( is_plugin_active('woocommerce/woocommerce.php') ) {
+  			echo "<br>";
+  			echo "woocommerce is active";
+  			echo "<br>";
+		}
+}
+
+/**
+ * Generamos la dependencia con woocommerce
+ * 
+ * @access public
+ * @return void
+ */
+function register_required_plugins() {
+    // Variables
+    $plugins  = array(
+        // Mi plugin o tema necesita del plugin WP REST API para funcionar correctamente
+        array(
+            'name'               => 'woocommerce',  // El nombre del plugin.
+            'slug'               => 'woocommerce',     // El "slug" del plugin (normalmente el nombre de la carpeta).
+            'required'           => true            // Si es falso, el plugin es "recomendado" en lugar de "requerido".
+        )
+    );
+
+    // LLamar a la función de la librería
+    // que se encargará de las dependencias.
+    tgmpa( $plugins);
+}
+add_action( 'tgmpa_register', 'register_required_plugins' );
+
+
--- controllers/adminPage/adminPage.php
+++ controllers/adminPage/adminPage.php
@@ -1,20 +1,42 @@
 <?php
 
-class WOD_main_controller{
+namespace Controllers\adminPage;
+
+class WodMainController{
 
              // Here initialize our namespace and resource name.
     public function __construct() {
     
-        $this->namespace     = '/my-namespace/v1';
+        $this->namespace     = '/woocommerce-orders-downloader/';
         $this->resource_name = 'posts';
 
     }
 
+    // public function register_routes() {
+    //     register_rest_route( $this->namespace, '/' . $this->resource_name, array(
+    //         // Here we register the readable endpoint for collections.
+    //         array(
+    //             'methods'   => 'GET',
+    //             'callback'  => array( $this, ' my_admin_page_contents' ),
+    //             'permission_callback' => array( $this, 'get_items_permissions_check' ),
+    //         ),
+    //         // Register our schema callback.
+    //         'schema' => array( $this, 'get_item_schema' ),
+    //     ) );
+    // }
+
+
+    // function get_items_permissions_check(){}
+
 }
 // Function to register our new routes from the controller.
 function prefix_register_my_rest_routes() {
-    $controller = new WOD_main_controller();
+    $controller = new WodMainController();
     $controller->register_routes();
 }
 
 add_action( 'rest_api_init', 'prefix_register_my_rest_routes' );
+
+add_action( 'init', function(){
+	$class = new WodMainController();
+});
